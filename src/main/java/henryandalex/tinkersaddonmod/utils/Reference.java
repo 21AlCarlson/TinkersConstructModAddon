df452611dbf013a1be009203accc439abc09cc16
Alex Carlson <adcmyphone@gmail.com>
2021-06-12 21:44:23 -0500

    Commit

---
+++ src/main/java/henryandalex/tinkersaddonmod/utils/Reference.java
@@ -0,0 +1,8 @@
+package henryandalex.tinkersaddonmod.utils;
+
+public class Reference {
+	public static final String MODID = "tinkersaddon";
+	public static final String NAME = "Tinkers_Addon";
+	// Pre-Alpha
+	public static final String VERSION = "0.0.1";
+}
