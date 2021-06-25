3b49752a58cc4d9c1df7d54b48dd89aa103d21cf
Alex Carlson <adcmyphone@gmail.com>
2021-06-24 19:39:57 -0500

    Pre-Release Commit (Registering Material to Tinkers)

--- src/main/java/henryandalex/tinkersaddonmod/utils/handlers/RegistryHandler.java
+++ src/main/java/henryandalex/tinkersaddonmod/utils/handlers/RegistryHandler.java
@@ -2,6 +2,10 @@ package henryandalex.tinkersaddonmod.utils.handlers;
 
 import henryandalex.tinkersaddonmod.init.ItemInit;
 import henryandalex.tinkersaddonmod.utils.IHasModel;
+
+import slimeknights.tconstruct.common.TinkerOredict;
+
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -16,6 +20,17 @@ public class RegistryHandler {
 		event.getRegistry().registerAll(ItemInit.ITEMS.toArray(new Item[0]));
 	}
 	
+	/**
+	 * Registers all the blocks and item oredicts.
+	 * Note that it's using the item registry event, since it's called after blocks.
+	 * This relies on the TinkerOredict pulse being called after the pulses registering the items
+	 * See TinerOredict for initial usage.
+	 */
+	@SubscribeEvent
+	public void registerItems(RegistryEvent.Register<Item> event) {
+		registerCommon();
+	}
+
 	@SubscribeEvent
 	public static void onModelRegister(ModelRegistryEvent event) {
 		for(Item item : ItemInit.ITEMS) {
@@ -23,5 +38,10 @@ public class RegistryHandler {
 				((IHasModel) item).registerModels();
 			}
 		}
+	}
+	
+	
+	private void registerCommon() {
+		TinkerOredict.oredict(Items.LEATHER, "leather");
 	}
 }
