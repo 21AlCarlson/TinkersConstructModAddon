8cac32008a0e980600e002d23634575fa7adb247
Alex Carlson <adcmyphone@gmail.com>
2021-06-30 16:56:16 -0500

    added some new blocks and items!

--- src/main/java/henryandalex/tinkersaddonmod/init/BlockInit.java
+++ src/main/java/henryandalex/tinkersaddonmod/init/BlockInit.java
@@ -16,4 +16,14 @@ public class BlockInit {
 	
 	
 	public static final Block BLOCK_TUNGSTEN = new BlockBase("block_tungsten", Material.IRON);
+			
+	
+	public static final Block BLOCK_ORE_TUNGSTEN = new BlockBase("block_ore_tungsten", Material.IRON);
+	
+	public static final Block WITCHS_WOOD = new BlockBase("witchs_wood", Material.WOOD);
+	
+	public static final Block STONE_BRICK = new BlockBase("stone_brick", Material.ROCK);
+	
+	public static final Block WATER_WASH = new BlockBase("water_wash", Material.IRON);
+
 }
