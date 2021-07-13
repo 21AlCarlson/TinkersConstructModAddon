91cee61bd58b309832efa59b8f213b26f2a037c7
henrydavis88 <henryjdavis88@gmail.com>
2021-07-13 10:41:04 -0500

    grASS :)

--- src/main/java/henryandalex/tinkersaddonmod/objects/blocks/BlockWitchGrass.java
+++ src/main/java/henryandalex/tinkersaddonmod/objects/blocks/BlockWitchGrass.java
@@ -8,6 +8,7 @@ import henryandalex.tinkersaddonmod.init.ItemInit;
 import henryandalex.tinkersaddonmod.utils.IHasModel;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDirt;
+import net.minecraft.block.BlockGrass;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
@@ -24,6 +25,9 @@ public class BlockWitchGrass extends Block implements IHasModel {
 		setUnlocalizedName(name);
 		setRegistryName(name);
 		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		
+        this.setTickRandomly(true);
+
 	
 		BlockInit.BLOCKS.add(this);
 		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
@@ -36,15 +40,15 @@ public class BlockWitchGrass extends Block implements IHasModel {
 		TCAddonMod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");	
 	}
 
-	
+	@Override
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand)
     {
-        if (!worldIn.isRemote)
+		if (!worldIn.isRemote)
         {
-            if (!worldIn.isAreaLoaded(pos, 3)) return; // Forge: prevent loading unloaded chunks when checking neighbor's light and spreading
+            if (!worldIn.isAreaLoaded(pos, 2)) return; // Forge: prevent loading unloaded chunks when checking neighbor's light and spreading
             if (worldIn.getLightFromNeighbors(pos.up()) < 4 && worldIn.getBlockState(pos.up()).getLightOpacity(worldIn, pos.up()) > 2)
             {
-                worldIn.setBlockState(pos, Blocks.DIRT.getDefaultState());
+                worldIn.setBlockState(pos, Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT));
             }
             else
             {
@@ -53,19 +57,13 @@ public class BlockWitchGrass extends Block implements IHasModel {
                     for (int i = 0; i < 4; ++i)
                     {
                         BlockPos blockpos = pos.add(rand.nextInt(3) - 1, rand.nextInt(5) - 3, rand.nextInt(3) - 1);
+                        IBlockState iblockstate = worldIn.getBlockState(blockpos);
+                        IBlockState iblockstate1 = worldIn.getBlockState(blockpos.up());
 
-                        if (blockpos.getY() >= 0 && blockpos.getY() < 256 && !worldIn.isBlockLoaded(blockpos))
+                        if (iblockstate.getBlock() == Blocks.DIRT && iblockstate.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT && worldIn.getLightFromNeighbors(blockpos.up()) >= 4 && iblockstate1.getLightOpacity(worldIn, blockpos.up()) <= 2)
                         {
-                            return;
-                        }
-
-                        IBlockState iblockstate = worldIn.getBlockState(blockpos.up());
-                        IBlockState iblockstate1 = worldIn.getBlockState(blockpos);
-
-                        if (iblockstate1.getBlock() == Blocks.DIRT && iblockstate1.getValue(BlockDirt.VARIANT) == BlockDirt.DirtType.DIRT && worldIn.getLightFromNeighbors(blockpos.up()) >= 4 && iblockstate.getLightOpacity(worldIn, pos.up()) <= 2)
-                        {
-                            worldIn.setBlockState(blockpos, BlockInit.WITCHS_GRASS.getDefaultState());
-                        }
+                            worldIn.setBlockState(blockpos, this.getDefaultState());
+                        } 
                     }
                 }
             }
